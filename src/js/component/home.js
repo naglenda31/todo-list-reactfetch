4499d3ebd9dc6ed6edd4be0989bba57e383c4c26
naglenda31 <naglenda31@gmail.com>
2021-03-31 01:29:42 +0000

    starting project

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-export function Home() {
+export function TodoList() {
+	const [list, setList] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+	const handleKeyPress = e => {
+		if (e.key === "Enter" && inputValue !== "") {
+			setList(
+				list.concat({
+					label: inputValue,
+					done: false
+				})
+			);
+			setInputValue("");
+		}
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="">
+			<h1 className="text-center my-3 display-3">ToDos</h1>
+			<div className="container d-flex flex-column p-0">
+				<ul className="list-container list-unstyled">
+					<li>
+						<input
+							className="text-center"
+							type="text"
+							placeholder="Enter your tasks here"
+							onChange={e => setInputValue(e.target.value)}
+							onKeyPress={e => handleKeyPress(e)}
+							value={inputValue}
+						/>
+					</li>
+					{list.map((listItem, index) => (
+						<li key={index}>{listItem.label}</li>
+					))}
+				</ul>
+				<div className="total-items pl-2 ml-2">
+					{list.length === 0
+						? "You're done!!"
+						: list.length === 1
+						? list.length + " task to complete"
+						: list.length > 1
+						? list.length + " tasks to complete"
+						: list.length + " tasks to complete"}
+				</div>
+			</div>
 		</div>
 	);
 }
